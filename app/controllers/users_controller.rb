37063d467be38aaf04f198fc71ffd0a301862adb
Aliya <alia.mirzalieva@com>
2016-02-10 15:06:53 +0000

    Finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,37 @@
 class UsersController < ApplicationController
-  def index
+  
+  def new
+      @user = User.new
   end
-
-  def show
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Спасибо за регистрацию!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
-
+  
+  
   def edit
   end
-
-  def destroy
+  
+  def show
+      @user = User.find(params[:id])
   end
+  
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+  
+    
+  end 
+
+  
+
 end
