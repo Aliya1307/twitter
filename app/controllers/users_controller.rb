f8f738696aa7880b050484b103b7173727071fa0
Aliya <alia.mirzalieva@com>
2016-02-15 15:11:07 +0000

    User update, list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  before_action :sign_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+   
+  
   def new
       @user = User.new
   end
@@ -7,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in(@user)
       flash[:success] = "Спасибо за регистрацию!"
       redirect_to @user
     else
@@ -16,6 +21,21 @@ class UsersController < ApplicationController
   
   
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Данные обновлены"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+      @users = User.paginate(page: params[:page], per_page:10)
   end
   
   def show
@@ -32,6 +52,18 @@ class UsersController < ApplicationController
     
   end 
 
+  def sign_in_user
+    unless signed_in?
+      flash[:danger] = "Неободимо авторизоваться!"
+      store_location
+      redirect_to signin_path
+    end
   
 
+def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   
   resources :users
-  
+  resources :sessions, only: [:new, :create, :destroy]
+ 
   root 'static_pages#home'
   get "/about", to: "static_pages#about"
   match "/help", to: "static_pages#help", via: "get"
   match "/signup", to: "users#new", via: "get"
   match "/signup", to: "users#create", via: "post" 
-
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signin',  to: 'sessions#create',         via: 'post'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 
 end
